3793efb5180b3d41be2f198b52109bfe67910132
nielswitkamp <31725811+nielswitkamp@users.noreply.github.com>
2017-09-07 18:09:21 +0200

    laatste twee opdrachten les 2

---
+++ Les 2/pe2_3.py
@@ -0,0 +1,11 @@
+Vraag1 = 'vraag 1'
+Antwoord1 = (0 == (1 == 2))
+print Vraag1,Antwoord1
+
+Vraag2 = 'vraag 2'
+Antwoord2 = (2 + (3 == 4) + 5 == 7)
+print Vraag2,Antwoord2
+
+Vraag3 = 'vraag 3'
+Antwoord3 = ((1 < -1) == (3 > 4))
+print Vraag3,Antwoord3
---
+++ Les 2/pe2_4.py
@@ -0,0 +1,4 @@
+uurloon = float(input('wat verdient u per uur?'))
+uren = int(input('hoeveel uur heeft u gewerkt?'))
+line = uren ,', uur werken levert ' , float(uurloon)*float(uren) , ' euro op!'
+print(line)
