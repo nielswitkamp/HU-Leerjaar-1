5475ea5944d4ba2b66c2b8057e543ee6fc493d55
nielswitkamp <31725811+nielswitkamp@users.noreply.github.com>
2017-09-07 15:08:58 +0200

    xcvxc

---
+++ Les 2/pe2_1.py
@@ -0,0 +1,3 @@
+letters = ('A', 'C', 'B', 'B', 'C', 'A', 'C', 'C', 'B')
+letters2 = [letters.count('A') , letters.count('B') , letters.count('C')]
+print(letters2)
---
+++ Les 2/pe2_2.py
@@ -0,0 +1,9 @@
+cijferICOR = float(7)
+cijferPROG = float(7)
+cijferCSN = float(8.5)
+opgeteld = cijferCSN+cijferPROG+cijferICOR
+gemiddelde = opgeteld/3
+print(gemiddelde)
+beloning = cijferICOR*30+cijferPROG*30+cijferCSN*30
+print(beloning)
+overzicht = 'Mijn cijfers (gemiddeld een 7.5) leveren een beloning van € 675.0 op!'
